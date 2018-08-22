5a2a1fbe80f33885ff83f5ca99ed9bab93309f04
YH.Bai <602789299@qq.com>
2018-08-22 16:50:10 +0800

    change requirements

--- my_flask/__init__.py
+++ my_flask/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 app = Flask(__name__, static_folder='static')
 app.config.from_object(config['flask_db'])
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 def create_app():
     db.init_app(app)
